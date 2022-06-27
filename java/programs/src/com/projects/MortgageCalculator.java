7df8f912a3f0a5c119aaf78365a5f3a8e10f65ab
Raphael Tan <raphtan0901000@gmail.com>
2022-06-27 17:06:44 +0800

    Updated

--- java/programs/src/com/projects/MortgageCalculator.java
+++ java/programs/src/com/projects/MortgageCalculator.java
@@ -6,13 +6,36 @@ import java.util.Scanner;
 public class MortgageCalculator {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Principal Amount (in $): ");
-        double principal = scanner.nextDouble();
-        System.out.print("Annual Interest Rate (in %): ");
-        float rate = scanner.nextFloat()/(100*12);
-        System.out.print("Number of Payments (in Years): ");
-        int noPay = scanner.nextInt()*12;
+        double principal = 0;
+        float rate = 0;
+        int noPay = 0;
+        while (true) {
+            System.out.print("Principal Amount (in $): ");
+            principal = scanner.nextDouble();
+            if (principal >= 1_000 && principal <= 1_000_000){
+                break;
+            }
+            System.out.println("Enter a value between 1,000 and 1,000,000");
+        }
+        while (true){
+            System.out.println("Enter a value greater than 0 and less than or equal to 30");
+            System.out.print("Annual Interest Rate (in %): ");
+            rate = scanner.nextFloat();
+            if (rate > 0 && rate <= 30){
+                rate = rate/(100*12);
+                break;
+            }
+        }
+        while (true){
+            System.out.println("Enter a value between 1 and 30");
+            System.out.print("Number of Payments (in Years): ");
+            noPay = scanner.nextInt();
+            if (noPay >= 1 && noPay <=30) {
+                noPay = noPay * 12;
+                break;
+            }
+        }
         String mortgage = NumberFormat.getCurrencyInstance().format(principal*(rate*Math.pow((1+rate),noPay))/(Math.pow((1+rate),noPay)-1));
-        System.out.print("Mortgage Amount:" + mortgage);
+        System.out.print("Mortgage Amount: " + mortgage);
     }
 }
