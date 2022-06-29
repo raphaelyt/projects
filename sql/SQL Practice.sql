03ec940b10d55aaf9ef9c92999d1d99c78375145
Raphael Tan <raphtan0901000@gmail.com>
2022-06-29 15:31:31 +0800

    Added Questions 25 -35

--- sql/SQL Practice.sql
+++ sql/SQL Practice.sql
@@ -172,10 +172,101 @@ SELECT ROUND(SQRT(POWER(MAX(LAT_N)-MIN(LAT_N),2) + POWER(MAX(LONG_W) - MIN(LONG_
 FROM STATION
 ;
 
---26 
+--26 Type of Triangle
+SELECT CASE             
+            WHEN A + B > C AND B + C > A AND A + C > B THEN
+                CASE 
+                    WHEN A = B AND B = C THEN 'Equilateral'
+                    WHEN A = B OR B = C OR A = C THEN 'Isosceles'
+                    ELSE 'Scalene'
+                END
+            ELSE 'Not A Triangle'
+        END
+FROM TRIANGLES;
+
+--27 The PADS
+SELECT CONCAT(Name,'(',Substring(Occupation,1,1),')') AS Name
+FROM OCCUPATIONS
+ORDER BY Name ASC
+;
+SELECT CONCAT('There are  a total of ',COUNT(Occupation),' ',LOWER(Occupation),'s.') AS Total
+FROM OCCUPATIONS
+GROUP BY Occupation
+ORDER BY Total ASC
+;
+
+--28 Occupations
+set @r1=0, @r2=0, @r3=0, @r4=0;
+select min(Doctor), min(Professor), min(Singer), min(Actor)
+from(
+  select case when Occupation='Doctor' then (@r1:=@r1+1)
+            when Occupation='Professor' then (@r2:=@r2+1)
+            when Occupation='Singer' then (@r3:=@r3+1)
+            when Occupation='Actor' then (@r4:=@r4+1) end as RowNumber,
+    case when Occupation='Doctor' then Name end as Doctor,
+    case when Occupation='Professor' then Name end as Professor,
+    case when Occupation='Singer' then Name end as Singer,
+    case when Occupation='Actor' then Name end as Actor
+  from OCCUPATIONS
+  order by Name
+    ) temp
+group by RowNumber;
 
 
+--29 Binary Tree
+SELECT CASE
+    WHEN P IS NULL THEN CONCAT(N, ' Root')
+    WHEN N IN (SELECT DISTINCT P FROM BST) THEN CONCAT(N, ' Inner')
+    ELSE CONCAT(N, ' Leaf')
+    END
+FROM BST
+ORDER BY N ASC
+;
 
+--30 New Companies
+select c.company_code, 
+    c.founder, 
+    count(distinct e.lead_manager_code), 
+    count(distinct e.senior_manager_code), 
+    count(distinct e.manager_code), 
+    count(distinct e.employee_code)
+from company c
+    inner join employee e on e.company_code = c.company_code
+group by c.company_code,c.founder
+order by c.company_code;
+
+--31 Revising Aggregations - Averages
+SELECT
+    AVG(POPULATION)
+FROM CITY
+WHERE DISTRICT = 'California'
+;
+
+--32 Average Population
+SELECT
+    FLOOR(AVG(POPULATION))
+FROM CITY
+;
+
+--33 Japan Population
+SELECT SUM(POPULATION)
+FROM CITY
+WHERE COUNTRYCODE = 'JPN'
+;
+
+--34 Population Density Difference
+SELECT MAX(POPULATION) - MIN(POPULATION) as DIFFERENCE
+FROM CITY
+;
+
+--35 THE BLUNDER
+SELECT CEIL(AVG(Salary) - AVG(REPLACE(Salary, '0', '')))
+FROM EMPLOYEES
+WHERE SALARY > 1000
+    AND SALARY < POWER(10,5)
+;
+
+--36
 
 
 
